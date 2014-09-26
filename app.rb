66f1873430a0006b9069b0230e732f42d70a133b
Kat Padilla <hello@katpadi.ph>
2014-09-27 02:28:18 +0800

    read it from katpadi.ph

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 require 'sinatra'
 require 'haml'
+require 'open-uri'
 
 get '/' do
-  read_list()
+  read_from_katpadi()
+  follower_count()
   haml :index
 end
 
-def read_list
+# If local :P
+def read_list()
   @faves_list = File.open('faves.log', 'r').readlines
+end
+
+def read_from_katpadi()
+  @faves_list  = open('http://katpadi.ph/favelogs/faves.html') {|f| f.readlines }
+end
+
+def follower_count()
+  @followers  = open('http://katpadi.ph/favelogs/followers.html') {|f| f.readlines }
+  cunt = Array.new
+  followers.each do |line|
+    cunt.push(line.split.last)
+  end
+  @peak = cunt.max
 end
